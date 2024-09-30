c8d38582e56c70960e8c6b8809b25105ae43c5e3
SAlA-9 <SAlA-9@SI9-6.salas.local>
2024-09-30 09:21:08 -0500

    prueba commit

---
+++ pruebagit/src/pruebagit/Pruebagit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagit;
+
+/**
+ *
+ * @author SAlA-9
+ */
+public class Pruebagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+        // TODO code application logic here
+    }
+    
+}
